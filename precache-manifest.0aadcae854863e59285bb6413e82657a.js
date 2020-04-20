9d1abbead8f45aec89ab7304698c2ed61b6cd895
Vasyl <muran.wasyl@ukr.net>
2020-04-20 21:56:42 +0300

    Updates

---
+++ precache-manifest.0aadcae854863e59285bb6413e82657a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ba66ecdac2451425778110323f079d",
+    "url": "/router/index.html"
+  },
+  {
+    "revision": "2f8cfaca4ca8c75c9f97",
+    "url": "/router/static/css/main.fdc31e9d.chunk.css"
+  },
+  {
+    "revision": "87746034f68ef68250ca",
+    "url": "/router/static/js/2.825dede0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/router/static/js/2.825dede0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8cfaca4ca8c75c9f97",
+    "url": "/router/static/js/main.31dbc31b.chunk.js"
+  },
+  {
+    "revision": "6df8bc7ae0039100240c",
+    "url": "/router/static/js/runtime-main.3dcdd744.js"
+  },
+  {
+    "revision": "90679e0cfe8c59c45c061a82c48f01d4",
+    "url": "/router/static/media/Roboto-Regular.90679e0c.ttf"
+  },
+  {
+    "revision": "c3ec00650365a1a22662480b51ff315f",
+    "url": "/router/static/media/search.c3ec0065.svg"
+  }
+]);
